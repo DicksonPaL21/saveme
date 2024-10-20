d133bc7f3900181c1babbe13f8395f079fe63c68
DicksonPaL21 <dicksonpal_021@yahoo.com.ph>
2024-10-20 22:45:32 +0800

    Integrate buymeacoffee widget

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,6 +36,18 @@ export default function RootLayout({
         <Navbar />
         {children}
         <ArrowDown />
+        <script
+          data-name="BMC-Widget"
+          data-cfasync="false"
+          src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+          data-id="dicksonpal21"
+          data-description="Support me on Buy me a coffee!"
+          data-message="Love our app? Buy us a coffee! Your support keeps us going."
+          data-color="#5F7FFF"
+          data-position="Right"
+          data-x_margin="18"
+          data-y_margin="18"
+        />
       </body>
     </html>
   )
---
+++ src/components/BuyMeACoffeeButton/index.tsx
@@ -0,0 +1,17 @@
+const BuyMeACoffeeButton = () => {
+  return (
+    <a
+      href="https://www.buymeacoffee.com/dicksonpal21"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img
+        src="https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png"
+        alt="Buy Me A Coffee"
+        style={{ height: '60px', width: '217px' }}
+      />
+    </a>
+  )
+}
+
+export default BuyMeACoffeeButton
