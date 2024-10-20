fac01e3093e78d35bc2a3a3910864320fcf590c1
DicksonPaL21 <dicksonpal_021@yahoo.com.ph>
2024-10-20 21:40:50 +0800

    Update footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,61 +1,16 @@
-import Image from 'next/image'
+import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <footer className="mt-auto flex flex-wrap items-center justify-between gap-6 border-t py-24">
-      <p>
-        <span className="font-bold">
-          <span className="text-white">Save</span>
-          <span className="text-yellow-500">Me</span>
-        </span>{' '}
-        ©️ 2024. All Rights Reserved
-      </p>
-      <div className="flex gap-6">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+    <footer className="mt-auto flex flex-wrap items-center justify-between gap-6 border-t py-24 text-xs font-extralight leading-relaxed tracking-wide sm:text-sm">
+      <p>©️ 2024 SaveMe. All Rights Reserved</p>
+      <div className="flex gap-4">
+        <Link href="/terms-of-service" className="hover:text-yellow-500">
+          Terms of Service
+        </Link>
+        <Link href="/privacy-policy" className="hover:text-yellow-500">
+          Privacy Policy
+        </Link>
       </div>
     </footer>
   )
