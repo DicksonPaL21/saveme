06f6b7ea878305777a50d1e2b74b39e0b26ead0c
DicksonPaL21 <dicksonpal_021@yahoo.com.ph>
2024-10-22 14:12:59 +0800

    fix eslint error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import ArrowDown from '@/components/ArrowDown'
 import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
+import BmcWidget from '@/components/widget/Bmc'
 import { cn } from '@/lib/utils'
 import type { Metadata } from 'next'
 import localFont from 'next/font/local'
@@ -42,18 +43,7 @@ export default function RootLayout({
         {children}
         <Footer />
         <ArrowDown />
-        <script
-          data-name="BMC-Widget"
-          data-cfasync="false"
-          src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
-          data-id="dicksonpal21"
-          data-description="Support me on Buy me a coffee!"
-          data-message="Love our app? Buy us a coffee! Your support keeps us going."
-          data-color="#5F7FFF"
-          data-position="Right"
-          data-x_margin="18"
-          data-y_margin="18"
-        />
+        <BmcWidget />
       </body>
     </html>
   )
---
+++ src/components/widget/Bmc.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable */
+
+const BmcWidget = () => {
+  return (
+    <script
+      data-name="BMC-Widget"
+      data-cfasync="false"
+      src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+      data-id="dicksonpal21"
+      data-description="Support me on Buy me a coffee!"
+      data-message="Love our app? Buy us a coffee! Your support keeps us going."
+      data-color="#5F7FFF"
+      data-position="Right"
+      data-x_margin="18"
+      data-y_margin="18"
+    />
+  )
+}
+
+export default BmcWidget
