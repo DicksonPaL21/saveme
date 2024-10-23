d78d691d848c0f39407eada5f797656c0b9ff927
DicksonPaL21 <dicksonpal_021@yahoo.com.ph>
2024-10-23 22:31:19 +0800

    Add page-not-found handler

---
+++ src/app/page-not-found/index.tsx
@@ -0,0 +1,7 @@
+export default function PageNotFound() {
+  return (
+    <div className="container grid flex-grow place-items-center text-9xl font-extrabold leading-relaxed">
+      404
+    </div>
+  )
+}
