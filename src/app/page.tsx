2c33ef86f02d5c7d1134a54687dc32b97374faf1
DicksonPaL21 <dicksonpal_021@yahoo.com.ph>
2024-10-24 22:54:06 +0800

    Init home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,24 @@
-import Features from '@/components/Features'
-import FrequentlyAskedQuestions from '@/components/FrequentlyAskedQuestions'
-import HowToDownload from '@/components/HowToDownload'
-import More from '@/components/More'
-import PlaceholdersAndVanishInputDemo from '@/components/PlaceholdersAndVanishInputDemo'
-import SupportedResources from '@/components/SupportedResources'
-import { SVGs } from '@/components/ui/pulse-beam'
+import { BackgroundBeams } from '@/components/ui/background-beams'
 
 export default function Home() {
   return (
-    <main className="container mb-24 flex flex-col items-center gap-28">
-      <div className="relative flex h-[80vh] w-full items-center justify-center overflow-hidden">
-        <PlaceholdersAndVanishInputDemo />
-        <div className="absolute inset-0 flex items-center justify-center">
-          <SVGs />
+    <main className="container mb-24 flex min-h-[60dvh] flex-col items-center gap-28">
+      <BackgroundBeams className="top-0 z-0 h-[40rem]" />
+      <div className="grid h-[40rem] place-items-center">
+        <div className="z-10">
+          <h1 className="max-w-4xl bg-gradient-to-b from-zinc-800 to-zinc-950 bg-clip-text text-center font-sans text-xl font-bold text-transparent dark:from-zinc-50 dark:to-zinc-200 md:text-5xl">
+            Download videos effortlessly and watch them anytime, anywhere.
+          </h1>
+          <p className="mx-auto mt-4 max-w-xl text-center text-xs leading-relaxed tracking-wide text-neutral-500 sm:text-sm">
+            <span className="font-bold">SaveMe</span> is a web-based video
+            downloader application that allows users to effortlessly download
+            videos from popular platforms, including Facebook, YouTube, TikTok,
+            and Instagram Reels. With SaveMe, you can easily save your favorite
+            videos for offline viewing, making it a must-have tool for content
+            enthusiasts.
+          </p>
         </div>
       </div>
-      {/* <div className="bg-grid-small-black dark:bg-grid-small-white w-dvw py-28 shadow-[inset_0_0_60px_0_black]">
-        <div className="mx-auto flex w-full max-w-4xl flex-col">
-          <VideoWrapper />
-        </div>
-      </div> */}
-
-      <p className="mx-auto max-w-4xl text-center text-xs font-light leading-relaxed tracking-wide sm:text-base">
-        <span className="font-bold">SaveMe</span> is a web-based video
-        downloader application that allows users to effortlessly download videos
-        from popular platforms, including Facebook, YouTube, TikTok, and
-        Instagram Reels. With SaveMe, you can easily save your favorite videos
-        for offline viewing, making it a must-have tool for content enthusiasts.
-      </p>
-      <HowToDownload />
-      <Features />
-      <FrequentlyAskedQuestions />
-      <More />
-      <SupportedResources />
     </main>
   )
 }
